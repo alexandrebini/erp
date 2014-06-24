2673d15225082bd627dcb908be41c7f4fbe06966
Alexandre Bini <alexandrebini@gmail.com>
2014-06-23 22:23:36 -0300

    rolou...

--- config/initializers/simple_form_hack.rb
+++ config/initializers/simple_form_hack.rb
@@ -4,11 +4,24 @@ module SimpleForm
       def input_html_options
         p '---------------builder'
         p @builder.options[:wrapper]
-
+        # binding.pry
         # if @builder.options[:wrapper] == :bootstrap_angular
-          @input_html_options.reverse_merge!({ "ng-model" => "#{@builder.lookup_model_names.join(".")}.#{self.attribute_name}" })
+          @input_html_options.reverse_merge!({ 'ng-model' => ng_model, 'ng-init' => "#{ ng_model } = ''" })
         # end
         @input_html_options
+      end
+
+      def ng_model
+        @ng_model ||= begin
+          child_index = options[:child_index]
+          names = object_name.to_s.scan(/\w+/).flatten
+          names.delete(child_index) if child_index
+          names.each do |name|
+            name.gsub!('_attributes', '')
+            name.gsub!(name, "[#{ name }]") if name.match(/\d/)
+          end
+          "#{ names.join('.').gsub('.[', '[') }.#{ self.attribute_name }"
+        end
       end
     end
   end
